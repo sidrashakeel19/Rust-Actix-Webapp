a14c12b602b65d31e672a362da0119d9d5907994
home <sidrashakeel19@hotmail.com>
2020-08-20 23:16:10 +0500

    Added impl block and helper functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 // Necessary imports here
 use actix_web::{App, get, post, put, delete, HttpResponse, HttpServer};
 
+// -------------------- Global Variables ----------------------
+static mut AUTO_INCR_ID: i32 = 0;
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     // Initializing Server
@@ -42,7 +45,47 @@ struct Students {
     age: i32,
 }
 
+impl Students {
+    // Constructor function to get a new Student object
+    fn new(id: i32, first_name: String, last_name: String, department: String,
+    is_graduated: bool, age: i32) -> Students {
+        Students {
+            id,
+            first_name,
+            last_name,
+            department,
+            is_graduated,
+            age
+        }
+    }
+    // Function which transform Student object to Students object
+    fn from(student: Student) -> Students {
+        Students::new(get_id(), student.first_name, student.last_name, student.department,
+        student.is_graduated, student.age) 
+    }
+}
 // ------------------------ End Models ---------------------------
+
+// ---------------------- Helper Functions -----------------------
+
+// Function to increment id by 1 after each insertion 
+pub fn add_to_id() {
+    unsafe {
+        AUTO_INCR_ID +=1;
+    }
+}
+
+//Getter function to get Global ID
+pub fn get_id() -> i32 {
+    let mut id: i32;
+    unsafe {
+        add_to_id();
+        id = AUTO_INCR_ID;
+    }
+    id
+}
+
+// -------------------- End Helper Functions ---------------------
 
 // ----------------------- Route Handlers------------------------
 
